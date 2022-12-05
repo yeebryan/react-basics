89a7e1ebb480a6bf92ed5b3537b7b509c9bce71a
yeebryan <bryanwebiz@gmail.com>
2022-12-05 16:21:11 +0800

    update pics and styles and app.js

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # react-basics
+
+first open terminal and type following to create react app
+
+'npx create-react-app 01-lab'
+
+once project created, run 'cd 01-lab'
+THEN 'npm start'
+
+You should see the React logo loading on your port number on chrome
+
+----
+
+now open up src/app.js
+
+--- END OF exercise 1-lab: First React app --
--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; // import React
+import './styles.css'; // import css
 
+// create and update App function with some html
+// react.fragment makes it easier/efficient to write multiple element
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <h1>Hello Worlds!</h1>    
+      <p>Welcome to our first React App</p>
+      <img src={require('./logo.svg').default} alt ='logo'/>
+      <img src={require('./cat.jpeg').default} alt='cat'/>
+    </React.Fragment>
+    );
 }
+// anything in {} is javascript 
 
 export default App;
+
+// A React app consists of various components - a component
+// essentially a function that will return HTML output
+// The App component is what React app will display when u run it
